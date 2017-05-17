0939c5083d944d818b6a379ab229278aa7c50078
vinicius0809 <vinicius0809@gmail.com>
2017-05-17 13:48:00 -0300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .models import Funcionario, Permanencia
 
 # Create your views here.
 def index(request):
@@ -17,4 +18,9 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
+def permanencia(request):
+    func = Funcionario.objects.get(ra=1477048)
+    #print ("Funcionario nao encontrado")
+    return render(request, 'permanencia.html', {'permanencia': func})
 
